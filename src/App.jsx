d9605af42a5e00a8fc92684bcc25f8666347338d
iamcesarjr <engineer.cesardcjr@gmail.com>
2025-11-21 11:14:08 +0800

    Added Checkout routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
 import { Route, Routes } from "react-router";
 import "./App.css";
 
@@ -6,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<div>Test Checkout</div>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
