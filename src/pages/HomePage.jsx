026ebba30d0c974a5aa750e69749d1bc3ac15e63
iamcesarjr <engineer.cesardcjr@gmail.com>
2025-11-21 15:10:14 +0800

    Used data from the backend to generate product lists

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./HomePage.css";
 import Header from "../components/Header";
-import { products } from "../data/products";
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
   return (
     <>
       <title>Ecommerce Project</title>
