b808c4889a3c22bfc05887a79b74545c7812365b
iamcesarjr <engineer.cesardcjr@gmail.com>
2025-11-21 11:30:35 +0800

    Split Header in to component and use Link

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import "./CheckoutPage.css";
 import "./checkout-header.css";
 
@@ -8,10 +9,10 @@ function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/">
+            <Link to="/">
               <img className="logo" src="images/logo.png" />
               <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
